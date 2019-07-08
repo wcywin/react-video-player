40c7d74c381d69c760c13714e71e9f2f2103b39c
wcywin <wcywin@gmail.com>
2019-07-08 21:34:41 +0200

    applying styled-components

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import WbnPlayer from './WbnPlayer';
 import ErrorPage from '../ErrorPage';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalStyle from '../styles/GlobalStyle';
 
 const App = () => (
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={WbnPlayer} />
-            <Route exact path="/:activeVideo" component={WbnPlayer} />
-            <Route path="*" component={ErrorPage} />
-        </Switch>
+        <>
+            <Switch>
+                <Route exact path="/" component={WbnPlayer} />
+                <Route exact path="/:activeVideo" component={WbnPlayer} />
+                <Route path="*" component={ErrorPage} />
+            </Switch>
+            <GlobalStyle />
+        </>
     </BrowserRouter>
 );
 
