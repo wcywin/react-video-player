23903d5e3be01d6323aecc901a0ad5b6753baed6
wcywin <wcywin@gmail.com>
2019-07-08 21:18:39 +0200

    app structure

---
+++ src/components/containers/Playlist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PlaylistHeader from '../PlaylistHeader';
+import PlaylistItems from '../containers/PlaylistItems';
+import NightMode from '../NightMode';
+
+const Playlist = props => {
+    return (
+        <>
+            <NightMode />
+            <PlaylistHeader />
+            <PlaylistItems />
+        </>
+    )
+};
+
+export default Playlist;
